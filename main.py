209de0b852a0066518176d8b79d91032c208ee18
Robin Keskisärkkä <robin.keski@gmail.com>
2026-03-08 23:45:29 +0100

    generate

---
+++ main.py
@@ -0,0 +1,11 @@
+with open("index.html", "w") as out:
+    with open("README.md") as _in:
+        out.write(f"""<html>
+<body>
+<h1>Website README</h1>
+<div>
+{_in.read()}
+</div>
+</body>
+</html>"""
+        )
